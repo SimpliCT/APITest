8e85c0e6e8e97c99c74e52d9314fbdae178ca2be
Jacob Macbook <j.guldhammer2@hotmail.dk>
2024-04-30 11:19:44 +0200

    remove modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Modal from 'react-modal';
+
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator, Avatar } from '@chatscope/chat-ui-kit-react'
@@ -7,45 +7,15 @@ import logo from './assets/SimpliCT_logo.svg';
 import AvatarLogo from './assets/SimpliCT_bot_icon.svg';
 import userlogo from './assets/SimpliCT_user_icon.svg';
 
-Modal.setAppElement('#root');
-
 const systemMessage = {
   role: "system",
-  content: ""
+  content: "You are a tutor aiding help with teaching computation thinking"
 };
-
-/* TODO: Disable modal closing when clicking outside.
-   TODO: Remove border on modal.
-   TODO: Make user choose their experience/knowledge level after choosing field.
-         Figure out how to enable new prompt for this.
-*/
 
 function App() {
 
   const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([]);
-  const [modalIsOpen, setIsOpen] = useState(true);
-
-  // function handleCloseModal(systemMessageString) {
-  function handleCloseModal() {  
-    systemMessage.content = "You are a tutor aiding help with teaching computation thinking.";
-    // switch(systemMessageString) {
-    //   case "ComputerScience":
-    //     systemMessage.content = "Start every message with 'CS:'.";
-    //     break;
-    //   case "Health":
-    //     systemMessage.content = "Start every message with 'Health:'.";
-    //     break;
-    //   case "Politics":
-    //     systemMessage.content = "Start every message with 'Politics:'.";
-    //     break;
-    //   case "Other":
-    //     systemMessage.content = "You are a tutor aiding help with teaching computation thinking."; // Explain it in terms that a 15 year old would understand. You can be a bit sarcastic";
-    //     break;
-    // }
-
-    setIsOpen(false); //Closes modal after picking field
-  }
 
   const HandleSend = async (message) => {
     const newMessage = {
@@ -129,22 +99,6 @@ function App() {
             <MessageInput autoFocus={true} placeholder='Type message here' onSend={HandleSend}/> 
           </ChatContainer>
         </MainContainer> 
-        <Modal 
-          className='Modal'
-          overlayClassName='ModalOverlay'
-          isOpen={modalIsOpen}
-          onRequestClose={handleCloseModal}
-          contentLabel="Initial Question Overlay"
-        >
-          <h2>Please state the field in which you work</h2>
-          <div className='grid2x2'>
-            <div className="box box1"><button onClick={() => handleCloseModal("ComputerScience")}>Computer Science</button></div>
-            <div className="box box2"><button onClick={() => handleCloseModal("Health")}>Health</button></div>
-            <div className="box box3"><button onClick={() => handleCloseModal("Politics")}>Politics</button></div>
-            <div className="box box4"><button onClick={() => handleCloseModal("Other")}>Some Other Stuff</button></div>
-          </div>
-
-        </Modal>
     </div>
   )
 }
